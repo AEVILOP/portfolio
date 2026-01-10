aa17c57884c36ce069219f79218bbe95d424570a
Anirban Banerjee <132850276+AEVILOP@users.noreply.github.com>
2026-01-11 00:27:51 +0530

    1st

---
+++ README.txt
@@ -0,0 +1,13 @@
+# black #222831
+
+# Grey  #393E46
+
+# Teal  #00ADB5
+
+# white #EEEEEE
+
+Links
+
+linkedIn :https://www.linkedin.com/in/anirban-banerjee-7a131224a/
+X:https://x.com/Anirban4810
+gitHub : https://github.com/AEVILOP
---
+++ js/link.js
@@ -0,0 +1,55 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== HOME TYPED JS ===============*/
+const typedHome = new Typed('#home__typed', {
+    strings: ['Web Developer', 'Frontend Developer', 'React developer'],
+    typeSpeed: 90,
+    backspeed: 40,
+    backDelay: 2000,
+    cursorChar: '|'
+})
+
+
+/*=============== ADD SHADOW HEADER ===============*/
+
+
+/*=============== CONTACT EMAIL JS ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
